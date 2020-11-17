ad1b293d3ee84289f76eb58ad797d0922f49d998
Brian Han <brianhan@brians-mbp.lan>
2020-11-16 21:27:04 -0800

    finished creating, styling header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,21 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="scoreHeader">
-      <ul className="scoreHeaderTextContainer">
-        <li className="scoreHeaderText">Rock</li>
-        <li className="scoreHeaderText">Paper</li>
-        <li className="scoreHeaderText">Scissors</li>
-        <li className="scoreHeaderText">Lizard</li>
-        <li className="scoreHeaderText">Spock</li>
-      </ul>
-      <ul className="scoreHeaderScoreContainer">
-        <li className="scoreHeaderScoreTitle">Score</li>
-        <li className="scoreHeaderScore">12</li>
-      </ul>
+      <div className="scoreHeaderInnerContainer">
+        <ul className="scoreHeaderTextContainer">
+          <li className="scoreHeaderText">ROCK</li>
+          <li className="scoreHeaderText">PAPER</li>
+          <li className="scoreHeaderText">SCISSORS</li>
+          <li className="scoreHeaderText">LIZARD</li>
+          <li className="scoreHeaderText">SPOCK</li>
+        </ul>
+      </div>
+      <div className="scoreHeaderInnerContainer">
+        <ul className="scoreHeaderScoreContainer">
+          <li className="scoreHeaderScoreTitle">SCORE</li>
+          <li className="scoreHeaderScore">12</li>
+        </ul>
+      </div>
     </div>
   )
 }
