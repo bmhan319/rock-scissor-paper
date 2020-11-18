5f406e593e0d85261264149dc61a5af8a5bdeae6
Brian Han <brianhan@brians-mbp.lan>
2020-11-17 23:42:58 -0800

    cleaned up gameplay styling

--- src/components/GameNormal.js
+++ src/components/GameNormal.js
@@ -5,18 +5,18 @@ export default function GameNormal(props) {
   return (
     <div className="gameComponent">
       <NavLink to="/gameplay">
-        <div onClick={()=>{props.playerPick('paper')}} className="outerCircle paperCircle">
+        <div onClick={()=>{props.playerPick('paper')}} className="outerCircle outerCirclePos paperCircle paperCirclePos">
           <div className="innerCircle innerPaperCircle"></div>
         </div>
       </NavLink>
 
       <NavLink to="/gameplay">
-        <div onClick={()=>{props.playerPick('scissor')}} className="outerCircle scissorsCircle">
+        <div onClick={()=>{props.playerPick('scissor')}} className="outerCircle outerCirclePos scissorsCircle scissorsCirclePos">
           <div className="innerCircle innerScissorCircle"></div>
         </div>
       </NavLink>
       <NavLink to="/gameplay">
-        <div onClick={()=>{props.playerPick('rock')}} className="outerCircle rockCircle">
+        <div onClick={()=>{props.playerPick('rock')}} className="outerCircle outerCirclePos rockCircle rockCirclePos">
           <div className="innerCircle innerRockCircle"></div>
         </div>
       </NavLink>
